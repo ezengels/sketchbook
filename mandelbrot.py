10cb07c88f09545504ddb6dfeb5b272b0411b997
Ezra Engels <ezengels@iu.edu>
2024-01-18 00:29:06 -0500

    first sketch

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,8 +1,18 @@
 import numpy as np
+from imageio import imwrite
 
 m = 480
 n = 320
 
-x = np.linespace(-2, 1, num=m).reshape((1, m))
-y = np.linespace(-2, 1, num=n).reshape((n, 1))
+Z = np.zeros((n, m), dtype=complex)
+M =np.full((n,m), True, dtype=bool)
+
+x = np.linspace(-2, 1, num=m).reshape((1, m))
+y = np.linspace(-2, 1, num=n).reshape((n, 1))
 C = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
+
+for i in range(100):
+    Z[M] = Z[M] * Z[M] + C[M]
+    M[np.abs(Z) > 2] = False
+
+imwrite('mandelbrot.png', np.uint8(np.flipud(1 - M) * 255))
